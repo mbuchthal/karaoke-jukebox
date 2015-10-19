781ca88fa0e2f3cb707009a57a8199cf12703829
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-19 11:15:19 -0700

    correct file structure, correct gulp path

--- gulpfile.js
+++ gulpfile.js
@@ -31,13 +31,13 @@ gulp.task('servertests', function() {
 });
 
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./app/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./app/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('build', ['jshint', 'jscs:warn']);
