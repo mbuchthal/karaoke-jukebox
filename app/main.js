25db069a01cc6c8a4777b68db18a3d0f9f7ea300
Hollis Lau <hollislau@hotmail.com>
2015-10-20 00:44:22 -0700

    Add functionality to render upcoming lyrics; rearrange file and directory structure; remove repetitive code

--- app/main.js
+++ app/main.js
@@ -32936,82 +32936,88 @@
 
 	  angular.module("test").controller("LyricsCtrl", function() {
 	    var vm = this;
-	  }).directive("renderedLyric", ["$interval", function($interval) {
+	  }).directive("renderedLyrics", ["$interval", function($interval) {
 
-	    function link(scope, element, attrs) {
-	      var lyric = mp3.lyrics[0].text;
-	      var lineCounter = 0;
-	      var lyricArray = lyric.split("");
-	      // var renderArray, unrenderedArray;
-	      var renderArray = lyricArray.slice(0, 0);
-	      var unrenderArray = lyricArray.slice(0);
-	      var counter = 0;
+	    function link(scope, element) {
+	      var lyricArray, renderedArray, unrenderedArray;
+	      var currentLyric = mp3.lyrics[0].text;
 	      var beatDuration = mp3.lyrics[0].beat_duration;
 	      var beatIncrements = mp3.lyrics[0].beat_increments;
+	      var lineCounter = 0;
+	      var beatCounter = 0;
 	      var charCounter = 0;
 	      var timeoutId;
 
-	      // console.log(renderArray);
-	      // console.log(unrenderArray);
-
-	      scope.rendered = renderArray.join("");
-	      // scope.rendered = "test";
-	      scope.unrendered = unrenderArray.join("");
-
 	      function incrementCounters() {
-	        charCounter += beatIncrements[counter];
-	        counter++;
+	        charCounter += beatIncrements[beatCounter];
+	        beatCounter++;
 	      }
 
 	      function resetCounters() {
 	        charCounter = 0;
-	        counter = 0;
+	        beatCounter = 0;
 	      }
 
 	      function updateLyrics() {
-	        lyricArray = lyric.split("");
-	        renderArray = lyricArray.slice(0, charCounter);
-	        unrenderArray = lyricArray.slice(charCounter);
-	        scope.rendered = renderArray.join("");
-	        scope.unrendered = unrenderArray.join("");
+	        lyricArray = currentLyric.split("");
+	        renderedArray = lyricArray.slice(0, charCounter);
+	        unrenderedArray = lyricArray.slice(charCounter);
+	        scope.renderedCurrentLyric = renderedArray.join("");
+	        scope.unrenderedCurrentLyric = unrenderedArray.join("");
 	      }
 
-	      function renderLyric() {
-	        element.append('<span class="rendered">' + scope.rendered + '</span><span class="unrendered">' + scope.unrendered + '</span>');
+	      function renderCurrentLyric() {
+	        element.append('<li><span class="rendered">' + scope.renderedCurrentLyric + '</span><span class="unrendered">' + scope.unrenderedCurrentLyric + '</span></li>');
 	      }
 
-	      function clearLyric() {
+	      function renderNextLyric() {
+	        element.append('<li class="unrendered">' + scope.nextLyric + '</li>');
+	      }
+
+	      function clearRenderedLyrics() {
 	        element.empty();
 	      }
 
 	      // scope.$watch("rendered", function() {
 	      //   updateLyric();
 	      // });
-	      renderLyric();
+
+	      scope.nextLyric = mp3.lyrics[1].text;
+
+	      // Render initial conditions to screen
+	      updateLyrics();
+	      renderCurrentLyric();
+	      renderNextLyric();
 
 	      timeoutId = $interval(function() {
-	        // scope.rendered = "hi";
-	        if (counter < beatIncrements.length) {
-	          incrementCounters();
-	          updateLyrics();
-	          clearLyric();
-	          renderLyric();
-	          console.log(scope.rendered);
+	        // Deactivate timer interval after last lyric has rendered
+	        if (lineCounter === mp3.lyrics.length - 1 && beatCounter === beatIncrements.length) {
+	          $interval.cancel(timeoutId);
 	        }
-	        if (counter === beatIncrements.length) {
+	        // On each beat, update counters
+	        if (beatCounter < beatIncrements.length) {
+	          incrementCounters();
+	          console.log(scope.renderedCurrentLyric);
+	        }
+	        // Retrieve next line when current lyric has finished and reset counters
+	        if (lineCounter < mp3.lyrics.length - 1 && beatCounter === beatIncrements.length) {
 	          lineCounter++;
-	          if (lineCounter === mp3.lyrics.length - 1) {
-	            $interval.cancel(timeoutId);
+	          currentLyric = mp3.lyrics[lineCounter].text;
+	          if (lineCounter < mp3.lyrics.length - 1) {
+	            scope.nextLyric = mp3.lyrics[lineCounter + 1].text;
+	          } else {
+	            scope.nextLyric = "";
 	          }
-	          lyric = mp3.lyrics[lineCounter].text;
 	          beatIncrements = mp3.lyrics[lineCounter].beat_increments;
 	          beatDuration = mp3.lyrics[lineCounter].beat_duration;
 	          resetCounters();
-	          console.log(lyric, beatIncrements, beatDuration);
-	          updateLyrics();
-	          clearLyric();
-	          renderLyric();
+	          console.log(currentLyric, scope.nextLyric, beatIncrements, beatDuration);
 	        }
+	        // Update lyrics and render to screen
+	        updateLyrics();
+	        clearRenderedLyrics();
+	        renderCurrentLyric();
+	        renderNextLyric();
 	      }, beatDuration);
 	    }
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,18 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var webpack = require('webpack-stream');
+
+
+gulp.task('webpack', function() {
+  return gulp.src('./app/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'main.js',
+      }
+    }))
+    .pipe(gulp.dest('./app'));
+});
 
 gulp.task('jshint', function() {
   return gulp.src('./*.js')
@@ -22,6 +34,11 @@ gulp.task('test', function() {
     });
 });
 
-gulp.task('build', ['jshint', 'test']);
+gulp.task('webpack:watch', function() {
+  gulp.watch('./app/**/*.js', ['webpack']);
+});
 
 gulp.task('default', ['build']);
+
+gulp.task('build', ['jshint', 'test', 'webpack']);
+
