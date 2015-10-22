4931ff7c269b6d2543329c979df90e2b76485758
Hollis Lau <hollislau@hotmail.com>
2015-10-22 14:11:08 -0700

    Add audio player functionality to app

--- app/index.js
+++ app/index.js
@@ -4,5 +4,4 @@ require('./js/kvoxctrl.js');
 require('./js/kvoxformctrl.js');
 require('./js/queue.ctrl.js');
 require('./js/songbookctrl.js');
-// require('./js/renderer.dir.js');
 require('./js/renderer.ctrl.js');
