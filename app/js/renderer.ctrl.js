2006c52159b05a43d264db40590d55a791d52804
Hollis Lau <hollislau@hotmail.com>
2016-01-27 17:46:30 -0800

    Refactor renderer controller to improve efficiency and clarity; Adjust margins of render screen for better layout on widescreen views

--- app/js/renderer.ctrl.js
+++ app/js/renderer.ctrl.js
@@ -1,120 +1,20 @@
 require("../app.js");
 require("./url.filter.js");
 
-(function() {
+(function () {
 
   "use strict";
 
   angular.module("kvoxapp").controller("RendererCtrl", ["$interval", "$http", "$log", "$q", "socket", function($interval, $http, $log, $q, socket) {
     var vm = this;
-//     var queue = {
-//       user: {
-//         id: "57394578394579",
-//         nick: "Bob"
-//       },
-//       song: {"title":"Rainbow Connection","author":"Kermit the Frog","mp3file":"rainbowconnection-kermitthefrog.mp3","genre":"Musical",
-// "lyrics":[
-// {"text":"•  •  • • • •","beatDuration":"264","beatIncrements":[
-//     0,0,0,0, 1,0,0,0,0,0, 1,0,0,0,0,0, 1,0,0,0,0,0,
-// 1,0,0,0,0,0, 1,0,0,0,0,0, 1,0,0,0,0,0,
-// 1,0,0,1,0,0, 1,0,0,1,0,0, 1,0,0,1,0,0, 1,0,0,1,0,0]},
-// {"text":"Why are there so many","beatDuration":"264","beatIncrements":[
-// 3,0,4,0,0,6,3,3,2,0,0,0]},
-// {"text":"songs about rainbows","beatDuration":"264","beatIncrements":[
-// 5,0,0,3,3,0,5,0,4,0,0]},
-// {"text":"and what's on the other side?","beatDuration":"264","beatIncrements":[
-// 3,7,0,3,0,4,0,2,1,1,1,1,0,6,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"Rainbows are visions","beatDuration":"264","beatIncrements":[
-// 4,0,4,0,0,4,4,0,5,0,0]},
-// {"text":"but only illusions","beatDuration":"264","beatIncrements":[
-// 3,3,0,2,0,0,4,1,0,5,0,0]},
-// {"text":"and rainbows have nothing to hide","beatDuration":"264","beatIncrements":[
-// 3,5,0,4,0,5,0,3,5,0,0,0,3,5,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"So we've been told","beatDuration":"264","beatIncrements":[
-// 2,0,6,0,5,0,5,0]},
-// {"text":"and some choose to believe it","beatDuration":"264","beatIncrements":[
-// 3,0,5,0,7,0,3,0,3,0,5,3,0,0,0,0]},
-// {"text":"I know they're wrong","beatDuration":"264","beatIncrements":[
-// 1,0,5,0,8,6,0,0]},
-// {"text":"wait and see","beatDuration":"264","beatIncrements":[
-// 4,0,0,4,4,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"Someday we'll find it","beatDuration":"264","beatIncrements":[
-// 4,0,3,0,0,6,5,0,3,0,0]},
-// {"text":"the Rainbow Connection","beatDuration":"264","beatIncrements":[
-// 3,5,0,3,0,0,4,3,0,4,0,0]},
-// {"text":"the lovers, the dreamers, and me","beatDuration":"264","beatIncrements":[
-// 3,3,5,0,0,0,4,6,4,0,0,4,0,3,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"• • • • • •","beatDuration":"264","beatIncrements":[
-// 1,1,1,1,1,1,1,1,1,1,1,0]},
-// {"text":"Who said that every wish","beatDuration":"264","beatIncrements":[
-// 3,0,5,0,0,5,3,3,5,0,0]},
-// {"text":"would be heard and answered","beatDuration":"264","beatIncrements":[
-// 5,3,0,6,0,0,4,4,0,6,0,0]},
-// {"text":"when wished on the morning star?","beatDuration":"264","beatIncrements":[
-// 4,7,0,3,0,4,0,5,0,0,0,3,0,6,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"Somebody thought of that","beatDuration":"264","beatIncrements":[
-// 4,2,2,0,0,0,8,3,5,0,0]},
-// {"text":"and someone believed it","beatDuration":"264","beatIncrements":[
-// 3,5,0,3,0,0,3,6,0,3,0,0,0]},
-// {"text":"Look what it's done so far","beatDuration":"264","beatIncrements":[
-// 4,0,5,0,5,0,5,0,0,0,3,0,4,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"What's so amazing","beatDuration":"264","beatIncrements":[
-// 6,0,3,0,3,0,1,0,5,0]},
-// {"text":"that keeps us stargazing","beatDuration":"264","beatIncrements":[
-// 4,0,6,0,3,0,5,0,3,0,3,0,0]},
-// {"text":"and what do we think we might see?","beatDuration":"264","beatIncrements":[
-// 3,5,0,3,0,3,0,6,0,0,3,6,0,5,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"Someday we'll find it","beatDuration":"264","beatIncrements":[
-// 4,0,3,0,0,6,5,0,3,0,0]},
-// {"text":"the Rainbow Connection","beatDuration":"264","beatIncrements":[
-// 3,5,0,3,0,0,4,3,0,4,0,0]},
-// {"text":"the lovers, the dreamers, and me","beatDuration":"264","beatIncrements":[
-// 3,3,5,0,0,0,4,6,4,0,0,4,0,3,0,0,0,0,0]},
-// {"text":"All of us under its spell","beatDuration":"264","beatIncrements":[
-// 3,0,0,3,3,0,3,0,0,3,4,0,6,0,0,0]},
-// {"text":"We know that it's probably magic...","beatDuration":"264","beatIncrements":[
-// 2,0,5,0,0,5,5,0,5,0,2,0,2,0,4,0,0,0,0,0,2,0,0,0,0,0,1,0,1,0,1,0]},
-// {"text":"Have you been half-asleep","beatDuration":"264","beatIncrements":[
-// 4,0,4,0,5,0,5,3,4,0,0]},
-// {"text":"and have you heard voices?","beatDuration":"264","beatIncrements":[
-// 3,5,0,4,0,6,0,4,0,4,0,0,0]},
-// {"text":"I've heard them calling my name","beatDuration":"264","beatIncrements":[
-// 4,0,6,0,5,0,5,0,3,0,3,0,5,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"Is this the sweet sound","beatDuration":"264","beatIncrements":[
-// 2,0,5,0,0,4,6,0,6,0,0,0,0]},
-// {"text":"that called the young sailors?","beatDuration":"264","beatIncrements":[
-// 4,7,0,0,4,6,0,5,0,4,0,0]},
-// {"text":"The voice might be one and the same","beatDuration":"264","beatIncrements":[
-// 3,6,0,6,0,3,0,4,4,0,0,4,0,5,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"I've heard it","beatDuration":"264","beatIncrements":[
-// 4,0,6,0,3,0]},
-// {"text":"too many times to ignore it","beatDuration":"264","beatIncrements":[
-// 3,0,3,0,2,0,6,0,3,0,3,0,4,3,0,0,0]},
-// {"text":"it's something that I'm s'posed to be","beatDuration":"264","beatIncrements":[
-// 4,5,0,5,0,0,5,4,0,8,0,0,3,3,0,0,0,0,0,0,0,0,0,0,0]},
-// {"text":"Someday we'll find it","beatDuration":"264","beatIncrements":[
-// 4,0,3,0,6,0,5,0,3,0,0]},
-// {"text":"the Rainbow Connection","beatDuration":"264","beatIncrements":[
-// 3,5,0,4,0,0,3,3,0,4,0,0]},
-// {"text":"the lovers, the dreamers, and me","beatDuration":"264","beatIncrements":[
-// 3,4,4,0,0,0,4,6,4,0,0,4,0,3,0,0,0,0,0]},
-// {"text":"Laa da daa dee da daa dum","beatDuration":"264","beatIncrements":[
-// 3,0,0,3,4,0,4,0,0,3,4,0,4,0,0,0]},
-// {"text":"Laa daa da daa daa de daa ooo","beatDuration":"264","beatIncrements":[
-// 3,0,4,0,0,3,4,0,4,0,3,0,4,0,4,0,0,0,0,0,0,0,0,0,0,0]}]}
-//     };
-    // var username = queue.user;
-    // var song = queue.song;
-    var user, song, lyricArray, renderedArray, unrenderedArray, currentLyric, timeoutId, beatDuration, beatIncrements, lineCounter, beatCounter, charCounter;
+    var user, song, currentLyric, timeoutId, beatDuration, beatIncrements, lineCounter, beatCounter, charCounter;
     var audio = document.getElementById("player");
     var mp3Path = "http://localhost:5678/"
 
     function initialize() {
-      console.log(socket.queue[0]);
       user = socket.queue[0].user;
       song = socket.queue[0].song;
       vm.songSrc = mp3Path + song.mp3file;
-      console.log(socket.queue);
       userMessage(",", " you're up!");
       beatDuration = song.lyrics[0].beatDuration;
       beatIncrements = song.lyrics[0].beatIncrements;
@@ -156,17 +56,14 @@ require("./url.filter.js");
       initialize();
       currentLyric = song.lyrics[0].text;
       vm.nextLyric = song.lyrics[1].text;
-      vm.renderedCurrentLyric = "";
-      vm.unrenderedCurrentLyric = "";
+      vm.highlightedLyric = "";
+      vm.plainLyric = "";
       vm.nextLyric = "";
     }
 
     function updateLyrics() {
-      lyricArray = currentLyric.split("");
-      renderedArray = lyricArray.slice(0, charCounter);
-      unrenderedArray = lyricArray.slice(charCounter);
-      vm.renderedCurrentLyric = renderedArray.join("");
-      vm.unrenderedCurrentLyric = unrenderedArray.join("");
+      vm.highlightedLyric = currentLyric.slice(0, charCounter);
+      vm.plainLyric = currentLyric.slice(charCounter);
     }
 
     function renderSong() {
